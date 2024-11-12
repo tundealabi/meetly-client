0468aaefb29bcd2dc196e0312c13b00742b226ab
Tunde Alabi <tundealabi4780@gmail.com>
2024-11-12 13:42:55 +0100

    feat: add minimal suppport for PWA;

---
+++ src/app/manifest.ts
@@ -0,0 +1,32 @@
+import type { MetadataRoute } from 'next';
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'Meetly',
+    short_name: 'Meetly',
+    description: 'Bringing People Together, One Meeting at a Time',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#ffffff',
+    theme_color: '#000000',
+    icons: [
+      {
+        src: '/web-app-manifest-144x144.png',
+        sizes: '144x144',
+        type: 'image/png',
+      },
+      {
+        src: '/web-app-manifest-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+      {
+        src: '/web-app-manifest-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+    ],
+  };
+}
